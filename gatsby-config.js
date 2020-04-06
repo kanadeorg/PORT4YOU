583ba71a2c685fdd0f3bc9fd64f84d655e3c9a39
Kanade <andy_wang0625@hotmail.com>
2020-04-06 05:22:43 -0400

    Init Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,14 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve:  `gatsby-plugin-typescript`,
+      options:{
+        isTSX: true,
+        jsxPragma: `React`,
+        allExtensions: true,
+      }
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+import { Link } from "gatsby"
+
+// import Layout from "../components/layout"
+// import Image from "../components/image"
+// import SEO from "../components/seo"
+
+interface IndexState{
+  isLoading?:boolean,
+}
+
+class IndexPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Test</h1>
+      </div>
+    );
+  }
+}
+
+export default IndexPage
